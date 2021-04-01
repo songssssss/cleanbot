4ae9160ff82f71941e1fa1b7c2fa244660739d41
songssssss <45288885+songssssss@users.noreply.github.com>
2021-04-01 17:14:37 +0800

    Add files via upload

---
+++ R/Shinyreadonemerge.R
@@ -0,0 +1,33 @@
+#' A function
+#'
+#' @param file A string
+#' @param expr A variable in the dataframe
+#'
+#' @return A dataframe
+#' @import dplyr
+#' @import utils
+#' @import haven
+#' @import stringr
+#' @import writexl
+#' @export
+#'
+Shinyreadonemerge=function(xls=F,path,keyword,writexlsx,writesav){
+  setwd(path)
+  if (xls==F)
+    dir()%>%tolower%>%grepl(".xlsx",.)%>%dir()[.]->file
+  else
+    dir()%>%tolower%>%grepl(".xls",.)%>%dir()[.]->file
+
+  filelist=file%>%set_names()%>%tolower()%>%str_detect(tolower(keyword))%>%file[.]
+  print(filelist)
+  alldata=list()
+  if (xls==F){
+    alldata=filelist%>%set_names()%>%map_df(readaexcel)}
+  else{
+    alldata=filelist%>%set_names()%>%map_df(readhtml)}
+  if(writexlsx==T)
+    alldata%>%writexl::write_xlsx(path = paste(keyword,".xlsx",sep = ""))
+  if(writesav==T)
+    alldata%>%haven::write_sav(path = paste(keyword,".sav",sep = ""))
+  return(alldata)
+}
---
+++ R/readnmerge.R
@@ -0,0 +1,59 @@
+#' A function
+#'
+#' @param file A string
+#' @param expr A variable in the dataframe
+#'
+#' @return A dataframe
+#' @import dplyr
+#' @import purrr
+#' @import rvest
+#' @import stringr
+#' @import writexl
+#' @import haven
+#' @import utils
+#' @export
+#need readsheets, readhtml functions
+readnmerge=function(html=F,writexlsx=F,writesav=F){
+  path=choose.dir(default = "", caption = "Select the folder where contains all raw data files.")
+  print(path)
+  setwd(path)
+
+  catno=readline("Number of categories: ")%>%as.integer()
+  keyword=vector(mode = "character",length = catno)
+  for (i in 1:catno)
+    keyword[i]=readline(prompt=paste("Enter keyword No.",i,": ",sep = ""))
+  print(keyword)
+  cat("Generating file list ...\n")
+
+  if (html==F)
+    dir()%>%tolower%>%grepl(".xlsx",.)%>%dir()[.]->file
+  else
+    dir()%>%tolower%>%grepl(".xls",.)%>%dir()[.]->file
+
+  filelist=file%>%set_names()%>%tolower()%>%
+    map_dfr(str_detect,tolower(keyword))%>%t()%>%`colnames<-`(keyword)%>%
+    apply(.,2,function(x)(which(x==T)%>%file[.]))
+  print(filelist)
+  if (class(filelist)=="character")
+    filelist=as.list(filelist)
+  else if (class(filelist)!="list")
+    filelist=lapply(seq_len(ncol(filelist)), function(i) filelist[,i])%>%`names<-`(keyword)
+  print(filelist)
+  cat("Processing...\n")
+
+  alldata=list()
+  for(i in seq_along(filelist)){
+    if (html==F){
+      alldata[[i]]=filelist[[i]]%>%set_names()%>%map_df(readaexcel)}
+    else{
+      alldata[[i]]=filelist[[i]]%>%set_names()%>%map_df(readhtml)}
+    if(writexlsx==T)
+      alldata[[i]]%>%writexl::write_xlsx(path = paste(names(filelist)[i],".xlsx",sep = ""))
+    if(writesav==T)
+      alldata[[i]]%>%haven::write_sav(path = paste(names(filelist)[i],".sav",sep = ""))
+    cat("Finished:",names(filelist)[i],"\n")
+  }
+
+  cat("Done\n")
+  return(alldata)
+}
