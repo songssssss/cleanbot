53e37bd758c66a6076736b1f580b301b68cb2ff2
songssssss <45288885+songssssss@users.noreply.github.com>
2021-04-07 14:36:00 +0800

    Update readnmerge.R

--- R/readnmerge.R
+++ R/readnmerge.R
@@ -47,10 +47,14 @@ readnmerge=function(html=F,writexlsx=F,writesav=F){
       alldata[[i]]=filelist[[i]]%>%set_names()%>%map_df(readaexcel)}
     else{
       alldata[[i]]=filelist[[i]]%>%set_names()%>%map_df(readhtml)}
-    if(writexlsx==T)
-      alldata[[i]]%>%writexl::write_xlsx(path = paste(names(filelist)[i],".xlsx",sep = ""))
-    if(writesav==T)
-      alldata[[i]]%>%haven::write_sav(path = paste(names(filelist)[i],".sav",sep = ""))
+  if(writexlsx==T){
+    maxno=1048000
+    lengthno=dim(alldata)[1]%/%maxno+1
+    for (i in 1:lengthno)
+      alldata[((i-1)*maxno+1):i*maxno,]%>%writexl::write_xlsx(path = paste(names(filelist),i,".xlsx",sep = ""))
+  }
+  if(writesav==T)
+    alldata[[i]]%>%haven::write_sav(path = paste(names(filelist)[i],".sav",sep = ""))
     cat("Finished:",names(filelist)[i],"\n")
   }
 
